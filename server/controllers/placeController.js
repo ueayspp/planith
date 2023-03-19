39d56cdb9be650e3d5e5b5044307b80a7685f0ef
Supapen K <auay.smiley@icloud.com>
2023-03-19 20:35:54 +0700

    feat(api): get durations between places

--- server/controllers/placeController.js
+++ server/controllers/placeController.js
@@ -1,22 +1,10 @@
 require('dotenv').config()
 
 const axios = require('axios')
-const { response } = require('express')
 
 const API_KEY = process.env.API_KEY
 const TEXT_SEARCH_URL = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
 const DIRECTIONS_URL = 'https://maps.googleapis.com/maps/api/directions/json'
-
-// GET all places
-const getPlaces = async (req, res) => {
-  res.json(['Get all places'])
-}
-
-// GET a single place
-const getPlace = async (req, res) => {
-  const { name } = req.params
-  res.json({ name })
-}
 
 // Search a place
 const searchPlace = async (req, res) => {
@@ -111,8 +99,6 @@ const getDurations = async (req, res) => {
 }
 
 module.exports = {
-  getPlaces,
-  getPlace,
   searchPlace,
   getDuration,
   getDurations,
--- server/routes/places.js
+++ server/routes/places.js
@@ -1,21 +1,9 @@
 require('dotenv').config()
 
 const express = require('express')
-const {
-  getPlaces,
-  getPlace,
-  searchPlace,
-  getDuration,
-  getDurations,
-} = require('../controllers/placeController')
+const { searchPlace, getDuration, getDurations } = require('../controllers/placeController')
 
 const router = express.Router()
-
-// GET all places
-// router.get('/', getPlaces)
-
-// GET a single place
-// router.get('/:name', getPlace)
 
 // Search a place
 router.get('/', searchPlace)
