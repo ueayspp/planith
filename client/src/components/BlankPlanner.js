c66979d90cb63a47fd5cf325a9dce3f4ecde91ca
Supapen K <auay.smiley@icloud.com>
2023-04-07 18:37:41 +0700

    feat: add blank planner component

---
+++ client/src/components/BlankPlanner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function BlankPlanner() {
+  return (
+    <div>
+      <p>
+        โอ๊ะโอ คุณยังไม่ได้เลือกสถานที่ <br /> กรุณาเลือกสถานที่ก่อนจัดแพลนเนอร์
+      </p>
+    </div>
+  )
+}
+
+export default BlankPlanner
